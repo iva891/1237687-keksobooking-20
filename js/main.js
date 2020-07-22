e53826104749d7804bffdda82436148d3bdb6c80
iva891 <57774593+iva891@users.noreply.github.com>
2020-07-22 21:17:14 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -106,6 +106,8 @@ insertPin(objects);
 // module4-task2
 
 var RADIUS_MAIN_PIN = 65;
+var MAX_ROOM = '100';
+var NOT_FOR_GUEST = '0';
 
 var formFieldsets = document.querySelectorAll('.ad-form fieldset');
 var pinMain = document.querySelector('.map__pin--main');
@@ -116,44 +118,52 @@ var formMain = document.querySelector('.ad-form');
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
 
-formFieldsets.forEach(function (elem) {
+var setDisable = function (elem) {
   elem.setAttribute('disabled', 'disabled');
-});
+};
+
+var removeDisable = function (elem) {
+  elem.removeAttribute('disabled');
+};
+
+formFieldsets.forEach(setDisable);
 
 var getActiveForm = function () {
   map.classList.remove('map--faded');
   address.setAttribute('value', Math.round(pinMainX + WIDTH_PIN / 2) + ', ' + Math.round(pinMainY + HEIGHT_PIN));
-  formFieldsets.forEach(function (elem) {
-    elem.removeAttribute('disabled');
-  });
+  formFieldsets.forEach(removeDisable);
 };
 
 address.setAttribute('value', pinMainX + Math.round(RADIUS_MAIN_PIN / 2) + ', ' + Math.round(pinMainY + RADIUS_MAIN_PIN / 2));
 
-pinMain.addEventListener('mousedown', function (evt) {
-  if (evt.button === 0) {
+var handleMouseClick = function (event) {
+  if (event.button === 0) {
     getActiveForm();
   }
-});
+};
 
-pinMain.addEventListener('keydown', function (evt) {
+var handleEnterPress = function (evt) {
   if (evt.key === 'Enter') {
     getActiveForm();
   }
-});
+};
+
+pinMain.addEventListener('mousedown', handleMouseClick);
+
+pinMain.addEventListener('keydown', handleEnterPress);
 
 var validateForm = function () {
   capacity.setCustomValidity('');
 
-  if ((roomNumber.value === '100') && (capacity.value !== '0')) {
+  if ((roomNumber.value === MAX_ROOM) && (capacity.value !== NOT_FOR_GUEST)) {
     capacity.setCustomValidity('100 комнат не для гостей');
   } else if (roomNumber.value < capacity.value) {
     capacity.setCustomValidity('Количество мест не должно превышать количество комнат');
-  } else if (roomNumber.value !== '100' && capacity.value === '0') {
+  } else if (roomNumber.value !== MAX_ROOM && capacity.value === NOT_FOR_GUEST) {
     capacity.setCustomValidity('Укажите количество гостей');
   }
 };
 
-formMain.addEventListener('input', validateForm());
+formMain.addEventListener('input', validateForm);
 
 
