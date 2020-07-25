80e74b73bb31bb9102ae729f878751d6c705f962
iva891 <57774593+iva891@users.noreply.github.com>
2020-07-26 02:20:59 +0300

    Исправление замечаний

--- js/load.js
+++ js/load.js
@@ -1,28 +1,38 @@
 'use strict';
 
 (function () {
+  var TYPE = 'json';
+  var SUCCESS_CODE = 200;
+  var MAX_TIMEOUT = 10000;
+
   window.load = function (url, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
+    xhr.responseType = TYPE;
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+    var onXhrLoad = function () {
+      if (xhr.status === SUCCESS_CODE) {
         onSuccess(xhr.response);
       } else {
         onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
-    });
+    };
 
-    xhr.addEventListener('error', function () {
+    var onXhrError = function () {
       onError('Произошла ошибка соединения');
-    });
+    };
 
-    xhr.addEventListener('timeout', function () {
+    var onXhrTimeout = function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+    };
 
-    xhr.timeout = 10000;
+    xhr.addEventListener('load', onXhrLoad);
+
+    xhr.addEventListener('error', onXhrError);
+
+    xhr.addEventListener('timeout', onXhrTimeout);
+
+    xhr.timeout = MAX_TIMEOUT;
 
     xhr.open('GET', url);
     xhr.send();
